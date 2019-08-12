40df8d5086da642e2cb4fdda4cbf1334c8238f4a
Alencia <alencia@Apples-MacBook-Pro-18.local>
2019-08-12 11:59:33 -0700

    message

---
+++ TablePractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TablePractice
+//
+//  Created by Alencia on 8/12/19.
+//  Copyright © 2019 Alencia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
